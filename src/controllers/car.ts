f5a4a94699e19a6ba97ef67b665cd24307c614ae
Tomasz Steć <tomeczekstecc@gmail.com>
2021-01-30 21:50:41 +0100

    e

---
+++ src/controllers/car.ts
@@ -0,0 +1,263 @@
+import { validate } from 'class-validator';
+import { Response } from 'express';
+import { Any } from 'typeorm';
+
+import { Submit, User } from '../entity';
+import { Car } from '../entity/Car';
+import { msg } from '../parts/messages';
+import { generatePdf, makeLog, saveRollbar } from '../services';
+import { mapErrors } from '../utils';
+
+const OBJECT: any = 'Submit';
+let ACTION, INFO: string, STATUS: string, CONTROLLER: string;
+
+//
+//
+//add a wnioski
+//
+
+export const addSubmit = async (req: any, res: Response) => {
+  let errors: any = {};
+  CONTROLLER = 'addSubmit';
+  ACTION = 'dodawanie';
+  try {
+    const user = await User.findOne({ id: req.session.userId });
+
+    if (!user) {
+      INFO = msg.client.fail.noUser;
+      STATUS = 'error';
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+
+      return res.status(400).json({
+        status: STATUS,
+        msgPL: INFO,
+      });
+    }
+
+    const peselExists = await Submit.find({ pupilPesel: req.body.pupilPesel });
+
+    if (peselExists.length > 0) {
+      errors.pupilPesel = msg.client.fail.peselExists;
+      // ****************************** LOG *********************************//
+      INFO = msg.client.fail.peselExists;
+      STATUS = 'error';
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+      // ********************************************************************//
+    }
+    if (Object.keys(errors).length > 0) {
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+
+      return res.status(400).json(errors);
+    }
+
+    const num = (await (await Submit.find()).length) + 10000;
+
+    const car = await Car.create({
+      ...req.body,
+      numer: `WN-${num}-v1-20`,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      pupilFirstName:
+        (req.body.isSelf === '1' && user.firstName) || req.body.pupilFirstName,
+      pupilLastName:
+        (req.body.isSelf === '1' && user.lastName) || req.body.pupilLastName,
+      pupilEmail:
+        (req.body.isSelf === '1' && user.email) || req.body.pupilEmail,
+
+      user,
+    });
+
+    errors = await validate(car);
+
+    if (errors.length > 0) {
+      // ****************************** LOG *********************************//
+      INFO = msg.client.fail.unvalidated;
+      STATUS = 'error';
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+      // ********************************************************************//
+      return res.status(400).json(mapErrors(errors));
+    }
+
+    await car.save();
+    // ****************************** LOG *********************************//
+    INFO = msg.client.ok.subCreated;
+    STATUS = 'success';
+    makeLog(OBJECT, car.id, ACTION, CONTROLLER, INFO, STATUS, req);
+    // ********************************************************************//
+
+    const data = {
+      car,
+    };
+    await generatePdf(data, 'car');
+
+    return res.status(201).json({
+      resStatus: 'success',
+      msgPL: INFO,
+      data: car,
+      alertTitle: 'Utworzono!',
+    });
+  } catch (err) {
+    STATUS = 'error';
+    saveRollbar(CONTROLLER, err.message, STATUS);
+    return res.status(500).json({
+      resStatus: STATUS,
+      msgPL: msg._500,
+      msg: err.message,
+      alertTitle: 'Błąd',
+    });
+  }
+};
+
+//
+//
+//edit a wnioski
+//
+
+export const editSubmit = async (req: any, res: Response) => {
+  CONTROLLER = 'editSubmit';
+  ACTION = 'edytowanie';
+  try {
+    const user = await User.findOne({ id: req.session.userId });
+
+    if (!user) {
+      // ****************************** LOG *********************************//
+      INFO = msg.client.fail.noUser;
+      STATUS = 'error';
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+      // ********************************************************************//
+
+      return res.status(400).json({
+        status: STATUS,
+        msgPL: INFO,
+      });
+    }
+
+    const tempSubmit = await Submit.create({ ...req.body }); // jako tymczasowy bo update nie ma save() i nie można walidować przed zapisem do bazy
+
+    const errors = await validate(tempSubmit);
+
+    if (errors.length > 0) {
+      // ****************************** LOG *********************************//
+      INFO = msg.client.fail.unvalidated;
+      STATUS = 'error';
+      makeLog(OBJECT, undefined, ACTION, CONTROLLER, INFO, STATUS, req);
+      // ********************************************************************//
+
+      return res.status(400).json(mapErrors(errors));
+    }
+    const submit = await Submit.update(
+      { uuid: req.body.uuid },
+      {
+        ...req.body,
+        pupilFirstName:
+          (req.body.isSelf === '1' && user.firstName) ||
+          req.body.pupilFirstName,
+        pupilLastName:
+          (req.body.isSelf === '1' && user.lastName) || req.body.pupilLastName,
+        pupilEmail:
+          (req.body.isSelf === '1' && user.email) || req.body.pupilEmail,
+      }
+    );
+
+    // ****************************** LOG *********************************//
+    INFO = msg.client.ok.subUpdated;
+    STATUS = 'success';
+    makeLog(OBJECT, tempSubmit.id, ACTION, CONTROLLER, INFO, STATUS, req);
+    // ********************************************************************//
+    const data = {
+      tempSubmit,
+    };
+    await generatePdf(data, 'submit');
+    return res.status(201).json({
+      resStatus: STATUS,
+      msgPL: INFO,
+      data: submit,
+      alertTitle: 'Zaktualizowano!',
+    });
+  } catch (err) {
+    STATUS = 'error';
+    saveRollbar(CONTROLLER, err.message, STATUS);
+    return res.status(500).json({
+      resStatus: STATUS,
+      msgPL: msg._500,
+      msg: err.message,
+      alertTitle: 'Błąd',
+    });
+  }
+};
+
+//
+//get all submits
+//
+export const getAllSubmits = async (req: any, res: Response) => {
+  try {
+    const submits = await Submit.find({ relations: ['user'] });
+
+    return res.status(200).json({
+      resStatus: 'success',
+      msgPL: msg.client.ok.subsFetched,
+      count: submits.length,
+      data: submits,
+    });
+  } catch (err) {
+    STATUS = 'error';
+    saveRollbar(CONTROLLER, err.message, STATUS);
+    return res.status(500).json({
+      resStatus: STATUS,
+      msgPL: msg._500,
+      msg: err.message,
+      alertTitle: 'Błąd!',
+    });
+  }
+};
+
+export const getAllUsersSubmits = async (req: any, res: Response) => {
+
+  try {
+    const submits = await Submit.find({
+      where: { userId: req.session.userId },
+    });
+
+    return res.status(200).json({
+      Status: 'success',
+      msgPL: msg.client.ok.subsFetched,
+      count: submits.length,
+      data:submits,
+    });
+  } catch (err) {
+    STATUS = 'error';
+    saveRollbar(CONTROLLER, err.message, STATUS);
+    return res.status(500).json({
+      resStatus: STATUS,
+      msgPL: msg._500,
+      msg: err.message,
+      alertTitle: 'Błąd',
+    });
+  }
+};
+
+export const getOneUserSubmit = async (req: any, res: Response) => {
+  const { uuid } = req.params;
+
+  try {
+    const submit = await Submit.findOne({ where: { uuid } });
+
+    return res.status(200).json({
+      resStatus: 'success',
+      msgPL: msg.client.ok.subFetched,
+      msg: 'Fetched submit',
+
+      submit,
+    });
+  } catch (err) {
+    STATUS = 'error';
+    saveRollbar(CONTROLLER, err.message, STATUS);
+    return res.status(500).json({
+      resStatus: STATUS,
+      msgPL: msg._500,
+      msg: err.message,
+      alertTitle: 'Błąd!',
+    });
+  }
+};
---
+++ src/entity/Car.ts
@@ -0,0 +1,247 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsIn,
+  IsInt,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  BeforeInsert,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
+
+import Model from './Model';
+import { User } from './User';
+
+import {
+  counselorProfileTypeEnums,
+  voyevEnums,
+  schoolTypeEnums,
+  priGradesEnums,
+  allTotalAvgEnums,
+} from './types';
+
+@Entity('cars')
+export class Car extends Model {
+  constructor(car: Partial<Car>) {
+    super();
+    Object.assign(this, car);
+  }
+
+  @Column({ comment: 'Wersja wniosku', default: 1 })
+  ver: number;
+
+  //
+  //I. dane osobowe
+  //
+  @Column({
+    type: 'enum',
+    comment:
+      '1:Status prawny Wnioskodawcy - Pełnoletni uczeń, 0:Status prawny Wnioskodawcy - Rodzic ucznia',
+    enum: [0, 1],
+    nullable: true,
+  })
+  isSelf: number;
+
+  @Column({
+    nullable: true,
+    comment: 'Telefon wniskodawcy',
+  })
+  phone: string;
+
+  @IsEmail(undefined, { message: 'Email musi mieć poprawny format' })
+  @Column({ nullable: true, comment: 'Email wnioskodawcy' })
+  email: string;
+
+  @Column({ nullable: true, comment: 'Adres skrzynki ePuap wnioskodawcy' })
+  epuapAdr: string;
+
+  //
+  //  II. DANE OSOBOWE UCZNIA
+  //
+
+  @Column({ comment: 'Imię ucznia', nullable: true })
+  pupilFirstName: string;
+
+  @Length(1, 254, {
+    message: 'Nazwisko ucznia musi zawierać od 1 do 254 znaków',
+  })
+  @Column({ comment: 'Nazwisko ucznia', nullable: true })
+  pupilLastName: string;
+
+  @Column({ comment: 'PESEL ucznia', nullable: true })
+  pupilPesel: string;
+
+  @Column({ comment: 'Telefon ucznia', nullable: true })
+  pupilPhone: string;
+
+  @Column({ nullable: true, comment: 'Email ucznia' })
+  pupilEmail: string;
+
+  //
+  //  III. DANE DOTYCZĄCE SZKOŁY PONADPODSTAWOWEJ ZNAJDUJĄCEJ SIĘ NA TERENIE
+  //
+
+  @Column({ comment: 'Nazwa szkoły ucznia', nullable: true })
+  schoolName: string;
+
+  @Column({ comment: 'Ulica szkoły ucznia', nullable: true })
+  schoolStreetName: string;
+
+  @Column({ comment: 'Numer ulicy szkoły ucznia', nullable: true })
+  schoolStreetNr: string;
+
+  @Column({ comment: 'Miejscowość szkoły ucznia', nullable: true })
+  schoolTown: string;
+
+  @Column({ comment: 'Kod pocztowy szkoły ucznia', nullable: true })
+  schoolZip: string;
+
+  @Column({
+    comment: 'Województwo szkoły ucznia',
+    type: 'enum',
+    enum: voyevEnums,
+    nullable: true,
+  })
+  schoolVoyev: string;
+
+  @Column({
+    type: 'enum',
+    enum: schoolTypeEnums,
+    comment: 'Typ szkoły ucznia',
+    nullable: true,
+  })
+  schoolType: string;
+
+  //
+  //  IV. DANE KANDYDATA NA OPIEKUNA DYDAKTYCZNEGO STYPENDYSTY
+  //
+
+  @Column({ comment: 'Imię opiekuna dydaktycznego ucznia', nullable: true })
+  counselorFirstName: string;
+
+  @Column({ comment: 'Nazwisko opiekuna dydaktycznego ucznia', nullable: true })
+  counselorLastName: string;
+
+  @Column({
+    type: 'enum',
+    enum: counselorProfileTypeEnums,
+    comment: 'Profil opiekuna dydaktycznego',
+    nullable: true,
+  })
+  counselorProfile: string;
+
+  //
+  // V.  PODSTAWOWE KRYTERIA OCENY
+  //
+
+  @Column({
+    type: 'enum',
+    enum: priGradesEnums,
+    comment: 'Podstawowe kryteria oceny - ocena z matematyki',
+    nullable: true,
+  })
+  priMathGrade: number;
+
+  @Column({
+    comment: 'Podstawowe kryteria oceny - nazwa języka obcego',
+    nullable: true,
+  })
+  priLang: string;
+
+  @Column({
+    type: 'enum',
+    enum: priGradesEnums,
+    comment: 'Podstawowe kryteria oceny - ocena z wybranego języka',
+    nullable: true,
+  })
+  priLangGrade: number;
+
+  @Column({
+    comment:
+      'Podstawowe kryteria oceny - nazwa wybranego przedmiotu kluczowego',
+    nullable: true,
+  })
+  priOtherSubj: string;
+
+  @Column({
+    type: 'enum',
+    enum: priGradesEnums,
+    comment: 'Podstawowe kryteria oceny - ocena z wybranego przedmiotu',
+    nullable: true,
+  })
+  priOtherSubjGrade: number;
+
+  @Column({
+    comment: 'Podstawowe kryteria oceny - średnia z przedmiotów kierunkowych',
+    nullable: true,
+  })
+  priTotalAver: number;
+
+  @Column({
+    type: 'enum',
+    enum: allTotalAvgEnums,
+    comment: 'Podstawowe kryteria oceny - średnia z wszystkich przedmiotów',
+    nullable: true,
+  })
+  allTotalAver: string;
+
+  //
+  // V.  DDODATKOWE KRYTERIA OCENY
+  //
+
+  @Column({
+    type: 'enum',
+    comment: 'Dodatkowe krytria oceny - czy uzyskał/a tytuł finalisty',
+    enum: [0, 1],
+    nullable: true,
+  })
+  isFinalist: boolean;
+
+  @Column({
+    type: 'enum',
+    comment:
+      'Dodatkowe krytria oceny - czy posiada zezwolenie na indyw. program nauczania',
+    enum: [0, 1],
+    nullable: true,
+  })
+  isAllowed: boolean;
+
+  @Column({
+    type: 'enum',
+    comment: 'Dodatkowe krytria oceny - czy uczeń/uczennica nbiepełnosprawny/a',
+    enum: [0, 1],
+    nullable: true,
+  })
+  isHandicap: boolean;
+
+  @Column({ comment: 'Suma kontrolna powiązanego pliku pdf', nullable: true })
+  checksum: string;
+
+  @Column({ comment: 'UuuidV4 użytkownika' })
+  userUuid: string;
+
+  @Column({ comment: 'ID użytkownika' })
+  userId: number;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'userId', referencedColumnName: 'id' })
+  @JoinColumn({ name: 'userUuid', referencedColumnName: 'uuid' })
+  user: User;
+
+  // @BeforeInsert()
+  // calculatePriAver() {
+  //   this.priTotalAver = Math.round(
+  //     (this.priMathGrade + this.priLangGrade + this.priOtherSubjGrade) / 3
+  //   );
+  // }
+}

670d1472fc55727c5d10e318621b43bf56983718
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-01 22:30:33 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -40,7 +40,7 @@ export const createApp = (store: Store) => {
   app.use(
     cors({
       credentials: true,
-      origin: 'http://localhost:3000',
+      origin: 'https://slaskietalenty.com',
       optionsSuccessStatus: 200,
     })
   );

61aeb5ba27ec94c69fa9165a54eddd540dda8855
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-02 18:06:26 +0100

    f

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -4,7 +4,7 @@ const { SMTP_USERNAME, SMTP_PASSWORD, SMTP_HOST, SMTP_PORT } = process.env;
 
 export const SMTP_OPTIONS: Options = {
   host: SMTP_HOST,
-  port: 143,
+  port: 465,
   secure: true,
   auth: {
     user: SMTP_USERNAME,

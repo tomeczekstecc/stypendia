e260256b78cdfab9768f6bfaf120c0ddd1920784
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-03 13:08:15 +0100

    e

--- src/config/session.ts
+++ src/config/session.ts
@@ -24,7 +24,7 @@ export const SESSION_OPTIONS: SessionOptions = {
   name: SESSION_NAME,
   cookie: {
     maxAge: +SESSION_IDLE_TIMEOUT,
-    secure: true,
+    secure: false,
     sameSite: false,
     path: SESSION_PATH,
   },

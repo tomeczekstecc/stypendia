cd183879e855c638388b0703528d24b8e84a3a0f
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-03 21:33:26 +0100

    e

--- client/src/components/styles/layout.styles.js
+++ client/src/components/styles/layout.styles.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  .grid {
-    /* width: 90%; */
-    position: relative;
-    overflow: hidden;
-    margin: auto;
-  }
+
 `;
--- client/src/components/styles/nav.styles.js
+++ client/src/components/styles/nav.styles.js
@@ -8,15 +8,15 @@ export const Wrapper = styled.div`
   opacity: 0.9;
 
   .submit {
-    display: ${(props) => (props.mode === 'edit' ? 'none' : null)};
+    display: ${(props) => (props.mode === 'edit' ? 'none !important' : null)};
   }
 
   .save {
-    display: ${(props) => (props.mode === 'new' ? 'none' : null)};
+    display: ${(props) => (props.mode === 'new' ? 'none !important' : null)};
   }
   .save,
   .submit {
-    display: ${(props) => (props.mode === 'watch' ? 'none' : null)};
+    display: ${(props) => (props.mode === 'watch' ? 'none !important' : null)};
   }
 
 `;

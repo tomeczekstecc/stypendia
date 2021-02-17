4c9a14b0c6b2dec4ebf6a62ee2262408bca496e8
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-17 21:50:37 +0100

    cookies-banner

--- client/src/components/commonLayout/Layout.js
+++ client/src/components/commonLayout/Layout.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Grid } from 'semantic-ui-react';
+import CookieConsent from 'react-cookie-consent';
 import MainMenu from './MainMenu';
 import { Wrapper } from '../styles/layout.styles';
 import Timer from '../Timer';
@@ -14,8 +15,39 @@ const Layout = ({ children }) => {
       <MainMenu />
       <Grid centered verticalAlign='middle'>
         {children}
-        {isLoggedIn && <Timer/>}
+        {isLoggedIn && <Timer />}
       </Grid>
+
+
+      <CookieConsent
+        location='bottom'
+        buttonText='Ok, rozumiem'
+        cookieName='styp_cookies_consest'
+        style={{ background: '#1578c2', fontSize: '16px' }}
+        buttonStyle={{
+          color: '#4e503b',
+          borderRadius: '3px',
+          padding: '8px 12px',
+          fontFamily: 'Lato, sans-serif',
+          fontSize: '16px',
+          background: '#CACBCD',
+          fontWeight: 'bold',
+        }}
+        expires={150}
+      >
+        Strona i jej elementy wykorzystują pliki cookies m.in. w celu
+        poprawienia jej dostępności, personalizacji, obsługi kont użytkowników.
+        Każdy może sam decydować o tym czy dopuszcza pliki cookies, ustawiając
+        odpowiednio swoją przeglądarkę. Dowiedz się więcej na stronie{' '}
+
+        <a
+          className='cookies-link'
+          href='https://pl.wikipedia.org/wiki/HTTP_cookie'
+        >
+          https://pl.wikipedia.org/wiki/HTTP_cookie
+        </a>
+        <span style={{ fontSize: '10px' }}></span>
+      </CookieConsent>
     </Wrapper>
   );
 };
--- client/src/components/styles/layout.styles.js
+++ client/src/components/styles/layout.styles.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,700;1,300&display=swap');
 
+  .cookies-link {
+    color: #fff;
+    text-decoration: underline;
+    font-weight: 700;
+  }
+
+  .CookieConsent {
+    font-family: 'Lato', sans-serif !important;
+    line-height: 1.4;
+    letter-spacing: .5px;
+  }
 `;

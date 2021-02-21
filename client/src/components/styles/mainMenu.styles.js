137a12cdda8cc0a5d6bf338064d261dbed6795b4
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-21 18:02:26 +0100

    fix

--- client/src/components/styles/mainMenu.styles.js
+++ client/src/components/styles/mainMenu.styles.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
+
 export const Wrapper = styled.div`
-  .main-menu-el {
   position: fixed !important;
   top: 0 !important;
   left: 0 !important;
-  width: 100% !important;
-  z-index: 1000 !important;
+  min-width: 100% !important;
+  .main-menu-el {
+    position: fixed !important;
+    top: 0 !important;
+    left: 0 !important;
+    min-width: 100% !important;
+    z-index: 1000 !important;
   }
-
 `;

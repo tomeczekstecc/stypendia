a46bfe96bfdfeff539b4f0387f1a4867e70fdc8a
Tomasz Steć <tomeczekstecc@gmail.com>
2021-02-21 01:18:42 +0100

    edfwdw

--- client/src/components/styles/layout.styles.js
+++ client/src/components/styles/layout.styles.js
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,700;1,300&display=swap');
 
-.gidziol{
- /* padding: 10rem !important */
-}
   .cookies-link {
     color: #fff;
     text-decoration: underline;

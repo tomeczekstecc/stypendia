0b6aff52d470cd643d678eb5cff22621fb8a5fdc
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-07 11:29:34 +0100

    fix

--- ormconfig.js
+++ ormconfig.js
@@ -9,11 +9,11 @@ module.exports = {
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   // synchronize: process.env.NODE_ENV === 'development',
-  synchronize: true,
+  synchronize: false,
   // logging: process.env.NODE_ENV === 'development',
   logging: false,
   // entities: [rootDir + '/entity/**/*.{ts,js}'],
-  entities: [__dirname + '/entity/**/*.js}'],
+  entities: [rootDir + '/entity/**/*.js}'],
   migrations: [rootDir + '/migration/**/*.{ts,js}'],
   //   subscribers: rootDir + ['/subscriber/**/*.{.ts,.js}'],
 

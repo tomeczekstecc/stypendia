bcee0680d42c624ec75ca57ab90adcc13336b80c
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-07 21:05:23 +0100

    fix

---
+++ src/ormconfig.js
@@ -0,0 +1,26 @@
+// const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'build';
+const rootDir = 'build';
+
+module.exports = {
+  type: process.env.DB_DIALECT,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  // synchronize: process.env.NODE_ENV === 'development',
+  synchronize: false,
+  // logging: process.env.NODE_ENV === 'development',
+  logging: false,
+  // entities: [rootDir + '/entity/**/*.{ts,js}'],
+  entities: [__dirname +'/entity/**/*.js}'],
+  migrations: [rootDir + '/migration/**/*.{ts,js}'],
+  //   subscribers: rootDir + ['/subscriber/**/*.{.ts,.js}'],
+
+  cli: {
+    migrationsDir: rootDir + '/migration',
+    // entitiesDir: rootDir + '/entity',
+    entitiesDir: __dirname + '/entity',
+    //  subscribersDir: rootDir + '/subscriber',
+  },
+};

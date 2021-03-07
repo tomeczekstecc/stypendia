9d4f7007b31b2da9edd742e9ea73715faa3f32ab
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-07 20:19:55 +0100

    fix

--- ormconfig.js
+++ ormconfig.js
@@ -13,7 +13,7 @@ module.exports = {
   // logging: process.env.NODE_ENV === 'development',
   logging: false,
   // entities: [rootDir + '/entity/**/*.{ts,js}'],
-  entities: [rootDir + '/entity/**/*.js}'],
+  entities: ['build/entity/**/*.js}'],
   migrations: [rootDir + '/migration/**/*.{ts,js}'],
   //   subscribers: rootDir + ['/subscriber/**/*.{.ts,.js}'],
 

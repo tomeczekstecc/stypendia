84e65ac3d3a94780723f29db32d9c4337b0bc933
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-07 21:20:53 +0100

    fix

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
-  password: process.env.DB,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   // synchronize: process.env.NODE_ENV === 'development',
   synchronize: true,

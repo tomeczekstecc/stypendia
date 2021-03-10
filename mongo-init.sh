400df455d4127c49e25681730a9b2757e295f9cd
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-10 09:00:14 +0100

    fix

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,2 +1,2 @@
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-db.createUser({user:"adminowski",pwd: "XPy4hPZt%B7?tjRr",roles: [{role: "readWrite", db: "logs"}]})
+db.createUser({user:"$MONGO_USERNAME",pwd: "$MONGO_PASSWORD",roles: [{role: "readWrite", db: "$MONGO_INITDB_DATABASE"}]})

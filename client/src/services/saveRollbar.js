470ab4b41064bfe86b1f5d362513a543e365f753
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-12 20:23:13 +0100

    tagged

--- client/src/services/saveRollbar.js
+++ client/src/services/saveRollbar.js
@@ -1,6 +1,6 @@
 // import Rollbar from 'rollbar';
 // const rollbar = new Rollbar({
-//   accessToken: '189797ea1c544c6c96500ce4ad734783',
+//   accessToken: ROLLBAR_FRONT_ACCESS_TOKEN,
 //   captureUncaught: true,
 //   captureUnhandledRejections: true,
 //   environment: process.env.NODE_ENV
--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ import {
 import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
-  accessToken: '8cfa68afd5104efb9192067f3eb1786a',
+  accessToken: process.env.ROLLBAR_BACKERND_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
   environment: process.env.NODE_ENV,

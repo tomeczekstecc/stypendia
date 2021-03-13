845b19266e106f003728af6c989c3f38baf094e0
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-13 10:26:58 +0100

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
-
+import { Beforeunload } from 'react-beforeunload';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Beforeunload onBeforeunload={(event) => event.preventDefault()}>
+      <App />
+    </Beforeunload>
   </React.StrictMode>,
+
   document.getElementById('root')
 );

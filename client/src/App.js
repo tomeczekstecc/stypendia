3d4a5c73aae4399b16be2478aa170afceb1abb5e
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-13 10:35:30 +0100

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -19,49 +19,54 @@ import {
 } from './pages';
 import { AuthState, AlertState, AppState, SubmitState } from './context';
 import { Alert } from './components';
+import { Beforeunload } from 'react-beforeunload';
 
 // axios.defaults.baseURL = 'http://localhost:5003';
 // axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <AlertState>
-      <AppState>
-        <Router>
-          <AuthState>
-            <SubmitState>
-              <Layout>
-                <Switch>
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/verify' component={Verify} />
-                  <Route exact path='/reset' component={Reset} />
-                  <Route exact path='/resetsend' component={ResetSend} />
-                  <Route exact path='/resend' component={Resend} />
-                  <Route exact path='/resend' component={Resend} />
+    <Beforeunload
+      onBeforeunload={(event) => 'Możliwa utrata niezapisanych danych!!!'}
+    >
+      <AlertState>
+        <AppState>
+          <Router>
+            <AuthState>
+              <SubmitState>
+                <Layout>
+                  <Switch>
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/verify' component={Verify} />
+                    <Route exact path='/reset' component={Reset} />
+                    <Route exact path='/resetsend' component={ResetSend} />
+                    <Route exact path='/resend' component={Resend} />
+                    <Route exact path='/resend' component={Resend} />
 
-                  <ProtectedRoute
-                    exact
-                    path='/changepass'
-                    component={ChangePass}
-                  />
-                  <Route exact path='/login' component={Login} />
-                  <ProtectedRoute exact path='/logout' component={Logout} />
+                    <ProtectedRoute
+                      exact
+                      path='/changepass'
+                      component={ChangePass}
+                    />
+                    <Route exact path='/login' component={Login} />
+                    <ProtectedRoute exact path='/logout' component={Logout} />
 
-                  <Route exact path='/submit' component={Submit} />
+                    <Route exact path='/submit' component={Submit} />
 
-                  <ProtectedRoute exact path='/' component={Home} />
+                    <ProtectedRoute exact path='/' component={Home} />
 
-                  <ProtectedRoute exact path='/profile' component={Profile} />
-                  <Route path='*' component={Page404} />
-                </Switch>
-              </Layout>
-            </SubmitState>
-          </AuthState>
+                    <ProtectedRoute exact path='/profile' component={Profile} />
+                    <Route path='*' component={Page404} />
+                  </Switch>
+                </Layout>
+              </SubmitState>
+            </AuthState>
 
-          <Alert position={'bottom-right'} autoDeleteInterval={10000} />
-        </Router>
-      </AppState>
-    </AlertState>
+            <Alert position={'bottom-right'} autoDeleteInterval={10000} />
+          </Router>
+        </AppState>
+      </AlertState>
+    </Beforeunload>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Beforeunload } from 'react-beforeunload';
+
 
 ReactDOM.render(
-  <Beforeunload
-    onBeforeunload={(event) => 'Możliwa utrata niezapisanych danych!!!'}
-  >
+
     <React.StrictMode>
       <App />
     </React.StrictMode>
     , document.getElementById('root')
-  </Beforeunload>
+
 );

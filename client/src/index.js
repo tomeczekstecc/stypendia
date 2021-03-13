a90602f49c5ed9cc8775cd50708b4562c681643f
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-13 10:23:20 +0100

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
-import { Beforeunload } from 'react-beforeunload';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Beforeunload onBeforeunload={() => "Czy na pewn? Odświeżenie strony może spowodować utratę danych"}> */}
-      <App />
-    {/* </Beforeunload> */}
+    <App />
   </React.StrictMode>,
-
   document.getElementById('root')
 );

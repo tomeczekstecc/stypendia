54687de6a33fa00b0c53cee0dec69fee02915af7
Tomasz Steć <tomeczekstecc@gmail.com>
2021-03-13 10:37:58 +0100

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -26,47 +26,48 @@ import { Beforeunload } from 'react-beforeunload';
 
 function App() {
   return (
-    <Beforeunload
-      onBeforeunload={(event) => 'Możliwa utrata niezapisanych danych!!!'}
-    >
-      <AlertState>
-        <AppState>
-          <Router>
-            <AuthState>
-              <SubmitState>
-                <Layout>
-                  <Switch>
-                    <Route exact path='/register' component={Register} />
-                    <Route exact path='/verify' component={Verify} />
-                    <Route exact path='/reset' component={Reset} />
-                    <Route exact path='/resetsend' component={ResetSend} />
-                    <Route exact path='/resend' component={Resend} />
-                    <Route exact path='/resend' component={Resend} />
+    <AlertState>
+      <AppState>
+        <Router>
+          <AuthState>
+            <SubmitState>
+              <Layout>
+                  <Beforeunload
+                    onBeforeunload={(event) =>
+                      'Możliwa utrata niezapisanych danych!!!'
+                    }
+                  />
+                <Switch>
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/verify' component={Verify} />
+                  <Route exact path='/reset' component={Reset} />
+                  <Route exact path='/resetsend' component={ResetSend} />
+                  <Route exact path='/resend' component={Resend} />
+                  <Route exact path='/resend' component={Resend} />
 
-                    <ProtectedRoute
-                      exact
-                      path='/changepass'
-                      component={ChangePass}
-                    />
-                    <Route exact path='/login' component={Login} />
-                    <ProtectedRoute exact path='/logout' component={Logout} />
+                  <ProtectedRoute
+                    exact
+                    path='/changepass'
+                    component={ChangePass}
+                  />
+                  <Route exact path='/login' component={Login} />
+                  <ProtectedRoute exact path='/logout' component={Logout} />
 
-                    <Route exact path='/submit' component={Submit} />
+                  <Route exact path='/submit' component={Submit} />
 
-                    <ProtectedRoute exact path='/' component={Home} />
+                  <ProtectedRoute exact path='/' component={Home} />
 
-                    <ProtectedRoute exact path='/profile' component={Profile} />
-                    <Route path='*' component={Page404} />
-                  </Switch>
-                </Layout>
-              </SubmitState>
-            </AuthState>
+                  <ProtectedRoute exact path='/profile' component={Profile} />
+                  <Route path='*' component={Page404} />
+                </Switch>
+              </Layout>
+            </SubmitState>
+          </AuthState>
 
-            <Alert position={'bottom-right'} autoDeleteInterval={10000} />
-          </Router>
-        </AppState>
-      </AlertState>
-    </Beforeunload>
+          <Alert position={'bottom-right'} autoDeleteInterval={10000} />
+        </Router>
+      </AppState>
+    </AlertState>
   );
 }
 

d121bdb2e2e5a9d3eec0127a259b21e5ad442a49
Tomasz Steć <tomeczekstecc@gmail.com>
2021-04-03 21:37:13 +0200

    docker

--- src/services/scanFile.ts
+++ src/services/scanFile.ts
@@ -1,30 +1,26 @@
-// import NodeClam from 'clamscan'
+import NodeClam from 'clamscan';
 
-// // clamscan module configs
-// const ClamScan = new NodeClam().init({
-//     debug_mode: true,
-//     scan_recursively: false,
-//     clamdscan: {
-//         socket: '/var/run/clamav/clamd.ctl',
-//         timeout: 120000,
-//         local_fallback: true,
-//         path: '/var/lib/clamav',
-//         config_file: '/etc/clamav/clamd.conf'
-//     },
-// });
+// clamscan module configs
+const ClamScan = new NodeClam().init({
+  clamdscan: {
+    socket: '/var/run/clamav/clamd.sock',
+    host: '127.0.0.1',
+    port: 3310,
+  },
+});
 
-// // Scan file using clamscan module
-// export function scanFile(filePath) {
-//   return ClamScan.then(async clamscan => {
-//     const { is_infected, viruses } = await clamscan.scan_file(filePath);
+// Scan file using clamscan module
+export function scanFile(filePath) {
+  return ClamScan.then(async (clamscan) => {
+    const { is_infected, viruses } = await clamscan.scan_file(filePath);
 
-//     if (is_infected) {
-//       console.log(`The file is INFECTED with ${viruses}`);
-//       throw new Error('ERR_FILE_SCAN_INFECTED');
-//     } else {
-//       return 'CLEAN';
-//     }
-//   }).catch(err => {
-//     throw new Error(err);
-//   });
-// }
+    if (is_infected) {
+      console.log(`The file is INFECTED with ${viruses}`);
+      throw new Error('ERR_FILE_SCAN_INFECTED');
+    } else {
+      return 'CLEAN';
+    }
+  }).catch((err) => {
+    throw new Error(err);
+  });
+}

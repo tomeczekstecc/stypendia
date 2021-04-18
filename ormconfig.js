994929576de945b2204bef29078e41aa0e94d87d
Tomasz Steć <tomeczekstecc@gmail.com>
2021-04-18 14:57:11 +0200

    test

--- ormconfig.js
+++ ormconfig.js
@@ -7,6 +7,7 @@ module.exports = {
   port: process.env.MYSQL_PORT,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  
   database: process.env.MYSQL_DATABASE,
   // synchronize: process.env.NODE_ENV === 'development',
   synchronize: true,
